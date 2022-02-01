f24e32a8212a562e5a5d3d1da6d15bfd81c9db39
Clay Webb <clay_webb@outlook.com>
2022-02-01 09:46:50 -0600

    Appending images to DOM, generating and displaying random name

--- client.js
+++ client.js
@@ -17,14 +17,32 @@ function addPictures() {
 	console.log("In addPictures");
 	for (let person of people) {
 		$("#pictures").append(`
-            <img class="correctImage" data-name="${person.name}" src="https://github.com/${person.githubUsername}.png?size=250" alt="Profile image of ${person.name}">
+        <img class="correctImage" data-name="${person.name}" src="https://github.com/${person.githubUsername}.png?size=250" alt="Profile image of ${person.name}">
         `);
-	}
+	} // End for of loop
+
+	// *** .forEach METHOD ------------------------------------------------------------------------
+	// people.forEach(function(){
+	//		$('#pictures').append(`
+//			<img class="correctImage" data-name="${person.name}" src="https://github.com/${person.githubUsername}.png?size=250" alt="Profile image of ${person.name}">
+//			`)
+//		})
+//	// *** .forEach METHOD ------------------------------------------------------------------------
+
 } // End addPictures function
 
 function randomNumber(min, max) {
 	return Math.floor(Math.random() * (1 + max - min) + min);
 }
+// ** ANOTHER METHOD TO FIND RANDOM NAME -----------------------------------------------------------------
+//	function chooseOne () {
+//		let randomNumber = Math.floor(Math.random() * (people.length); // <- Randomly select a person
+//		console.log('random number is', randomNumber); // <- Will console log a random number from array
+//		let chosenPerson = people[randomNumber].name; // <- 
+//		console.log('chosen person is', chosenPerson); // <- Will console log a random name from array
+//		$('#chosen-one').text(`Click on ${chosenPerson}'s face!`); // <- Will append to the DOM
+//	} // End chooseOne function
+// ** ANOTHER METHOD TO FIND RANDOM NAME -----------------------------------------------------------------
 
 function addRandomName() {
 	guessingName = people[randomNumber(0, people.length - 1)].name;
@@ -42,6 +60,4 @@ function correctPerson() {
 	}
 }
 
-// console.log('persons at', people[0]);
 
-// console.log("person at", people[randomNumber(0, people.length -1)]);
